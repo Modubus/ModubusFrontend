f0c9b55514ce0dcbdcf196f28ffeff9d7b78079e
rkdwoals159 <rkdwoals159@gmail.com>
2024-07-29 17:52:09 +0900

    fix : change appBar icon color to white

--- src/pages/onBoard/LoginPage.tsx
+++ src/pages/onBoard/LoginPage.tsx
@@ -6,7 +6,10 @@ type LoginPageParams = {
 };
 const LoginPage: ActivityComponentType<LoginPageParams> = () => {
   return (
-    <AppScreen backgroundColor="#121212" appBar={{ title: "" }}>
+    <AppScreen
+      backgroundColor="#121212"
+      appBar={{ title: "", iconColor: "white" }}
+    >
       <article className="w-full h-full flex flex-col justify-center">
         <section className="flex-1 flex flex-col w-full items-center justify-center gap-[1rem]"></section>
       </article>
