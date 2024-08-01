ff2019550252412b6f1a37c406c8f78f14dc0d07
inyoung <rmdnps10@gmail.com>
2024-08-02 01:11:18 +0900

    feat:Page declaration of driver home UI part

---
+++ src/pages/driver/busOngoing.tsx
@@ -0,0 +1,10 @@
+import { AppScreen } from "@stackflow/plugin-basic-ui";
+
+// 버스 운행 중 페이지
+export default function BusOngoingPage() {
+  return (
+    <AppScreen>
+      <div></div>
+    </AppScreen>
+  );
+}
---
+++ src/pages/driver/enrollBus.tsx
@@ -0,0 +1,10 @@
+import { AppScreen } from "@stackflow/plugin-basic-ui";
+
+// home(버기)1, 버스 검색&등록 페이지
+export default function EnrollBusPage() {
+  return (
+    <AppScreen>
+      <div></div>
+    </AppScreen>
+  );
+}
