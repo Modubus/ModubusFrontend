ad946cf936d506368c4e710096264f64d7bc914a
inyoung <rmdnps10@gmail.com>
2024-08-02 01:20:29 +0900

    feat: page declaration of driver onboarding part

---
+++ src/pages/onBoard/(driver)/EnterCompanyCode.tsx
@@ -0,0 +1,12 @@
+import { ActivityComponentType } from "@stackflow/react";
+import { AppScreen } from "@stackflow/plugin-basic-ui";
+
+const EnrollCompanyCodePage = () => {
+  return (
+    <AppScreen>
+      <div></div>
+    </AppScreen>
+  );
+};
+
+export default EnrollCompanyCodePage;
---
+++ src/pages/onBoard/(driver)/SearchCompanyCode.tsx
@@ -0,0 +1,12 @@
+import { ActivityComponentType } from "@stackflow/react";
+import { AppScreen } from "@stackflow/plugin-basic-ui";
+
+const SearchCompanyCodePage = () => {
+  return (
+    <AppScreen>
+      <div></div>
+    </AppScreen>
+  );
+};
+
+export default SearchCompanyCodePage;
