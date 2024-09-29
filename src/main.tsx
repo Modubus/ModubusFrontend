7afdb2038b705e160841642f5c9a20aaadf729d2
rkdwoals159 <rkdwoals159@gmail.com>
2024-09-29 23:07:03 +0900

    feat : add PWA setting

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "@src/App";
 import "@stackflow/plugin-basic-ui/index.css";
 import "@src/index.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
